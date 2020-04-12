584377fc1a4a1d97c26f519f1d467812833180b6
donut <kylecasey4@gmail.com>
2020-04-12 13:23:23 -0700

    dots

--- dotfiles/bin/disableKeypad.sh
+++ dotfiles/bin/disableKeypad.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 num=$(xinput | grep Tartarus | grep keyboard | grep "            " | cut -f 2 | cut -c 4-5)
 xinput --disable $num
-actkbd -s -d /dev/input/$(cat /proc/bus/input/devices | grep Razer -A 4 | grep leds | cut -d " " -f 4)
+actkbd -c /home/kyle/.actkbd.conf-s -d /dev/input/$(cat /proc/bus/input/devices | grep Razer -A 4 | grep leds | cut -d " " -f 4)
 
